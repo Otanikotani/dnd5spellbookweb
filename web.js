74f17b78b4a62c38cd5ce1ec2fd5bc37c6b66dae
hadzu <andrey.artyukhov@constant.obninsk.ru>
2015-01-08 11:30:26 +0300

    added bin dir

--- web.js
+++ web.js
@@ -1,8 +1,7 @@
-var gzippo = require('gzippo');
 var express = require('express');
-var morgan = require('morgan');
+
 var app = express();
 
-app.use(morgan('dev'));
 app.use(express.static("" + __dirname + "/bin"));
+app.use("/vendor", express.static(__dirname + '/bin/vendor'));
 app.listen(process.env.PORT || 5000);
