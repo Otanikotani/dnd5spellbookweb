cd1f9a34fcbb05e6e9a92f43eb5c4d88acd76a13
hadzu <andrey.artyukhov@constant.obninsk.ru>
2015-01-08 11:33:14 +0300

    added bin dir

--- web.js
+++ web.js
@@ -3,5 +3,4 @@ var express = require('express');
 var app = express();
 
 app.use(express.static("" + __dirname + "/bin"));
-app.use("/vendor", express.static(__dirname + '/bin/vendor'));
 app.listen(process.env.PORT || 5000);
